2228fbf380f3dfef30422f8d4b639135eb8242f9
amm <amm@amm-VirtualBox>
2015-03-12 18:22:00 +0100

    Prima versione del progetto

--- progetto/index.php
+++ progetto/index.php
@@ -10,7 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <h1>Ciao</h1>
+        <h1>Cia o</h1>
         <?php
         // put your code here
         ?>
